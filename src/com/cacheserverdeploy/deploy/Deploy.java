6629fc6e720b05e365dd33d610e4b23248e94b77
lucyking <joehisaishi1943@gmail.com>
2017-04-02 23:26:43 +0800

    +TreeMap::<int,int> comparable
    Signed-off-by: lucyking <joehisaishi1943@gmail.com>

--- src/com/cacheserverdeploy/deploy/Deploy.java
+++ src/com/cacheserverdeploy/deploy/Deploy.java
@@ -19,13 +19,15 @@ public class Deploy {
         int cNum = Integer.parseInt(detail[2]);
         int cost = Integer.parseInt(graphContent[2].trim());
         Queue sNodeCap;
+        TreeMap treeMap;
+        TreeMap<Double,Integer> tmap;
 
         int[][] capacity = new int[vNum][vNum];
         int[][] fee = new int[vNum][vNum];
 
         for (int i = 0; i < vNum; i++)
             for (int j = 0; j < vNum; j++)
-                capacity[i][j] = Integer.MAX_VALUE;
+                capacity[i][j] = 0; //Integer.MAX_VALUE;
 
 //        for(int i = 0;i < vNum ;i++)
 //            capacity[i][i] = 0;
@@ -40,6 +42,11 @@ public class Deploy {
             fee[start][end] = Integer.parseInt(edge[3]);
             fee[end][start] = fee[start][end];
         }
+
+//        sNodeCap = sumNodeCap(capacity);
+//        treeMap = sumNodeCap_TreeMap(capacity);
+        tmap = sumNodeCap_TreeMap(capacity);
+        printPairTreeMap(tmap);
 
         int[][] flowGraph = minFeeFlow(1, 13, 13, capacity, fee);
         printMatri(flowGraph);
@@ -323,10 +330,53 @@ public class Deploy {
                 sum += c[i][j];
             }
             re.add(new Pair(sum, i));
+            sum = 0;
         }
         return re;
     }
 
+    private static void printQueuePair(Queue<Pair> q){
+        Iterator<Pair> it  = q.iterator();
+        Pair tmp;// = new Pair();
+        while (it.hasNext()) {
+            tmp = it.next();
+            System.out.printf("<%d,%d>\n", tmp.first, tmp.second);
+        }
+    }
+
+    private static TreeMap sumNodeCap_TreeMap(int[][] c) {
+        int sum = 0;
+        TreeMap m = new TreeMap<Double,Integer>(new desPairCmp());
+        Pair p;
+        for (int i = 0; i < c.length; i++) {
+            for (int j = 0; j < c[0].length; j++) {
+                sum += c[i][j];
+            }
+            m.put(new Pair(sum, i),i);
+            sum = 0;
+        }
+        return m;
+    }
+
+    private static void printPairTreeMap(TreeMap map){
+
+        if (map == null)
+            return ;
+
+        System.out.println("\nIterator TreeMap By Entry Set:\n");
+        Pair key;
+        Integer val;
+        Iterator it = map.entrySet().iterator();
+        while(it.hasNext()) {
+            Map.Entry entry = (Map.Entry)it.next();
+
+            key = (Pair)entry.getKey();
+            val = (Integer)entry.getValue();
+            System.out.printf("<<%d,%d>%d>\n",key.first,key.second,val);
+        }
+
+
+    }
 }
 
 
